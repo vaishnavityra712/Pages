d33e1936bd6df363367f3b5eff965919a75546b2
VAISHNAVI ACHANTA <vaishnavityra712@gmail.com>
2021-07-13 20:27:54 +0530

    ok

--- Pages/settings.py
+++ Pages/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(r3(l)b)*-t*dub82o(t^&6j65yiy*oxdpb@^+0s=7$8s#8@pj'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
